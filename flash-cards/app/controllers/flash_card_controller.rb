81113175b32004c51b9fd89d12fd1fb0210b7020
Mike McTighe <m.mctighe10@gmail.com>
2019-11-02 20:13:29 +0000

    spec.md & edit

--- flash-cards/app/controllers/flash_card_controller.rb
+++ flash-cards/app/controllers/flash_card_controller.rb
@@ -10,6 +10,12 @@ class FlashCardController < ApplicationController
       erb :"flashcards/new"
     end
 
+    get '/flashcards/:id/edit' do
+      # @users = User.all
+      @flashcards = Flashcard.find_by_id(params[:id])
+      erb :"/flashcards/edit"
+    end
+
       get '/flashcards/:id' do
         @flashcards = Flashcard.find_by_id(params["id"])
         erb :"flashcards/show"
