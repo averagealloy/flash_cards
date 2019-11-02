58787b52c732f8c7f98b217a3b36ba04bc2d5ea4
Mike McTighe <m.mctighe10@gmail.com>
2019-11-02 17:56:41 +0000

    added a drop down for dummy data

--- flash-cards/app/controllers/flash_card_controller.rb
+++ flash-cards/app/controllers/flash_card_controller.rb
@@ -7,11 +7,14 @@ class FlashCardController < ApplicationController
 
     get '/flashcards/new'do
       erb :"flashcards/new"
-
     end
 
       get '/flashcards/:id' do
         @flashcards = Flashcard.find_by_id(params["id"])
         erb :"flashcards/show"
+      end
+
+      post '/post' do
+        binding.pry 
       end
 end
