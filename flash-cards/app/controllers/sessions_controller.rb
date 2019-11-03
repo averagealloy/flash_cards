4049cf3a70e6427e952b458e79d01daf5c90172a
Mike McTighe <m.mctighe10@gmail.com>
2019-11-03 01:50:16 +0000

    trying to fix login

--- flash-cards/app/controllers/sessions_controller.rb
+++ flash-cards/app/controllers/sessions_controller.rb
@@ -1,16 +1,17 @@
 class SessionsController < ApplicationController
 
-get '/login' do
-  erb :"sessions/login"
-end
+    get '/login' do
+      erb :"sessions/login"
+    end
 
-post "/login" do
-user = User.find_by(name: params[:name])
-if user.authenticate(params[:password])
-  session[:user_id] = user.id
-  redirect "/flashcards"
-else
-  redirect "/login"
-end
+    post "/login" do
+      user = User.find_by(name: params[:name])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/flashcards"
+      else
+        redirect "/login"
+      end
+    end
 
 end
--- flash-cards/db/seeds.rb
+++ flash-cards/db/seeds.rb
@@ -1,3 +1,3 @@
-10.times do 
+10.times do
     User.create(name: Faker::Name.name, email: Faker::Internet.email)
-end 
+end
