d8ba2ef4526e4ee10e3c58ab350ebd6fb6aa5d48
Mike McTighe <m.mctighe10@gmail.com>
2019-11-03 01:24:03 +0000

    found user by name then passed through if

--- flash-cards/app/controllers/sessions_controller.rb
+++ flash-cards/app/controllers/sessions_controller.rb
@@ -6,6 +6,11 @@ end
 
 post "/login" do
 user = User.find_by(name: params[:name])
+if user.authenticate(params[:password])
+  session[:user_id] = user.id
+  redirect "/flashcards"
+else
+  redirect "/login"
 end
 
 end
