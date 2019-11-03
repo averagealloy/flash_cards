33bd02fc04161572a68c5fdeee4b252c19889640
Mike McTighe <m.mctighe10@gmail.com>
2019-11-03 16:04:39 +0000

    making sure my email and name are uniq

--- flash-cards/app/models/user.rb
+++ flash-cards/app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
 
    has_many :flashcards
    has_secure_password
+
+   validates :name, uniqueness: true
+   validates :email, uniqueness: true
+   validates :name, presence: true
+   validates :email, presence: true
 end
